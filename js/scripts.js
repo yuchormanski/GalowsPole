34ccd4bf789e1521bad55172ccc6f272e8b49999
Nikolay Yuchormanski <yuchormanski@gmail.com>
2022-10-04 01:24:35 +0300

    up

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,13 @@ function buttonMaker() {
 	let words = fetchAllWords().toLowerCase().split(' ');
 	theWord = getRandom(words);
 	str = theWord.split('')
-	let first = str.shift();
-	let last = str.pop();
-	let counter = 0;
+	first = str.shift();
+	last = str.pop();
+	counter = 0;
 	let buff = '';
 	let allMixed;
+	wordCheck = str.join('')
+	console.log(wordCheck);
 
 
 	for (let i = 0; i < str.length; i++) {   // Create hidden letter places
@@ -17,34 +19,43 @@ function buttonMaker() {
 	}
 	allMixed = `${first} ${buff} ${last}`
 	document.getElementById("createButtons").innerHTML = allMixed;
-	//console.log(allMixed);    //debugging
-
 
 	// Input words list
 	function fetchAllWords() {
-		return 'заек Катерица Лисица Котка кон'// Катерица Лалугер Котка Невестулка Мишка Сръндак Козел Коза Подковонос';
+		return 'заек Катерица Лисица Котка кон Лалугер Невестулка Мишка Сръндак Козел Коза Подковонос лъв Вълк леопард Хиена мечка Гепард панда ';
 	}
 
 	// Randomize input words
 	function getRandom(list) {
 		return list[Math.floor((Math.random() * list.length))];
 	}
+
 }
 var fail = 0;
+forCheck = [];
+
 function check(el) {
-	//console.log(el);
+
 	let index = document.getElementById("letter" + el).name;
 	let char = document.getElementById("letter" + el).value;
 	let maxFailTurns = theWord.length - 2;
-	//console.log(char.charCodeAt());
-	//console.log(fail);
-	console.log(theWord.length);
+	let userCheck = '';
+
+	// IF char is Cyrillic 
 	if (char.charCodeAt() >= 1072 && char.charCodeAt() <= 1103) {
 
-		if (char == theWord[index]) {
-			document.getElementById("checks").innerHTML = "ok";
-		} else {
-			document.getElementById("checks").innerHTML = "no";
+		// CHAR success debugging
+		// if (char == theWord[index]) {
+		// 	document.getElementById("checks").innerHTML = "ok";
+		// } else {
+		// 	document.getElementById("checks").innerHTML = "no";
+		// }
+		//console.log(el);
+		//console.log(char.charCodeAt());
+		//console.log(fail);
+		//console.log(theWord.length);
+
+		if (char !== theWord[index]) {
 			fail++
 		}
 
@@ -52,26 +63,35 @@ function check(el) {
 		if (maxFailTurns === 1) {
 			if (char != theWord[index] && fail === 1) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/final3.png" alt=""/>`;
+				document.getElementById("response").classList = "galowsOn";
+			} else if (char === theWord[index]) {
+				forCheck.splice(index, 0, char);
 			}
-		// IF word has 2 letters to guess
+			// IF word has 2 letters to guess
 		} else if (maxFailTurns === 2) {
-			if (char != theWord[index] && fail === 1) {
+			if (char !== theWord[index] && fail === 1) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/three.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 2) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/final3.png" alt=""/>`;
+				document.getElementById("response").classList = "galowsOn";
+			} else if (char === theWord[index]) {
+				forCheck.splice(index, 0, char);
 			}
-		// IF word has 3 letters to guess
+			// IF word has 3 letters to guess
 		} else if (maxFailTurns === 3) {
-			if (char != theWord[index] && fail === 1) {
+			if (char !== theWord[index] && fail === 1) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/one.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 2) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/three.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 3) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/final3.png" alt=""/>`;
+				document.getElementById("response").classList = "galowsOn";
+			} else if (char === theWord[index]) {
+				forCheck.splice(index, 0, char);
 			}
-		// IF word has 4 letters to guess
+			// IF word has 4 letters to guess
 		} else if (maxFailTurns === 4) {
-			if (char != theWord[index] && fail === 1) {
+			if (char !== theWord[index] && fail === 1) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/one.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 2) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/three.png" alt=""/>`;
@@ -79,10 +99,13 @@ function check(el) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/four2.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 4) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/final3.png" alt=""/>`;
+				document.getElementById("response").classList = "galowsOn";
+			} else if (char === theWord[index]) {
+				forCheck.splice(index, 0, char);
 			}
-		// IF word has 5 and more letters to guess
-		} else if(maxFailTurns > 4){
-			if (char != theWord[index] && fail === 1) {
+			// IF word has 5 and more letters to guess
+		} else if (maxFailTurns > 4) {
+			if (char !== theWord[index] && fail === 1) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/one.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 2) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/two.png" alt=""/>`;
@@ -92,11 +115,23 @@ function check(el) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/four2.png" alt=""/>`;
 			} else if (char !== theWord[index] && fail === 5) {
 				document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/final3.png" alt=""/>`;
+				document.getElementById("response").classList = "galowsOn";
+			} else if (char === theWord[index]) {
+				forCheck.splice(index, 0, char);
 			}
 		}
 
+
+		// IF char is Latin letter
 	} else if (char.charCodeAt() >= 97 && char.charCodeAt() <= 122) {
 		alert("Използвай само букви на кирилица!")
+	}
+	userCheck = forCheck.join('');
+	console.log(userCheck);
+
+	if (wordCheck === userCheck) {
+		document.getElementById("yFail").innerHTML = `<img class="fullImage" src="img/winner.png" alt=""/>`;
+		//console.log('OK');
 	}
 }
 
